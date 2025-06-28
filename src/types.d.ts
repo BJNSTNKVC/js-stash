dfdd3e71052f444d4370abb6428b7eab76ff483e
Bojan Stankovic <dev.bojan.stankovic@gmail.com>
2025-06-28 21:46:07 +0200

    #13 Removed class export and module declaration.

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,11 +1,2 @@
-import { LocalStorage } from '@bjnstnkvc/local-storage';
-import { SessionStorage } from '@bjnstnkvc/session-storage';
 
-declare module '@bjnstnkvc/stash' {
-    export class Stash {
-        static driver(driver: 'local' | 'session'): LocalStorage | SessionStorage;
-        static local(): LocalStorage;
-        static session(): SessionStorage;
-    }
-}
 
